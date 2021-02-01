7fab286272184e4485d9df76ebf4c7ce30ede105
jenry valdes <jenryval93@gmail.com>
2021-01-31 18:50:09 -0600

    made the first API routes

--- Develop/server.js
+++ Develop/server.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = 3000
 
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 // app.get('/', (req , res) => {
@@ -20,9 +20,22 @@ app.get('/notes', (req , res) => {
    res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-app.get('*', (req , res) => {
+app.get('/', (req , res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
  });
+
+ app.get('/api/notes', (req , res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+ });
+
+// app.post('/api/notes' , (req, res) => {
+
+// })
+
+// app.delete('/api/notes/:id' , (req, res) => {
+
+
+// })
 
 
 
