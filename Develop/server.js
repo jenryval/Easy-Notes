8c34a3a8653dbc343efed60d23a5eb99c28a1f37
jenry valdes <jenryval93@gmail.com>
2021-01-31 18:27:15 -0600

    get pushes done

--- Develop/server.js
+++ Develop/server.js
@@ -7,14 +7,22 @@ const app = express();
 const PORT = 3000
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+
+// app.get('/', (req , res) => {
+//     res.send('hello bitches')
+// });
+
 
 app.get('/notes', (req , res) => {
-    res.sendFile(path.json(_dirname , 'notes.html'))
+   res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-
+app.get('*', (req , res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+ });
 
 
 
